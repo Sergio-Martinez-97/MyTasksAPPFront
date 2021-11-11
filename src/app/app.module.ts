1e8e3e994e549172dae79a8e9252813e4c5d8626
Sergio-Martinez-97 <sergiomartinez_1997@hotmail.com>
2021-11-11 15:40:04 +0100

    correcciones css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { AppComponent } from './app.component';
 import { TaskListComponent } from './task-list/task-list.component';
@@ -9,6 +10,7 @@ import { TaskDetailComponent } from './task-detail/task-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NewTaskComponent } from './new-task/new-task.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // @ts-ignore
 @NgModule({
@@ -23,7 +25,9 @@ import { NewTaskComponent } from './new-task/new-task.component';
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
